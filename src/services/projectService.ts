fbd2d18d13b72d4594eb5d537c1124bbb9dfd65f
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-13 17:57:29 +0500

    feat: add null check for workspaceFolders in getWorkspacePath method

--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -4,5 +4,9 @@ import * as vscode from 'vscode';
 export class ProjectService {
     getWorkspacePath(): string | undefined {
         const workspaceFolders = vscode.workspace.workspaceFolders;
+
+        if(!workspaceFolders) {
+            return undefined;
+        }
     }
 }
