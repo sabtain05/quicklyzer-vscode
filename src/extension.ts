6fdda5be6c00e867293b6e980e3b57d230ca938c
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-13 17:36:25 +0500

    fix: add workspaceFolders retrieval in analyzeProject command

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,9 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "quicklyzer" is now active!');
 
 	const disposable = vscode.commands.registerCommand('quicklyzer.analyzeProject', () => {
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+
+		
 		
 		vscode.window.showInformationMessage('Quicklyzer: Analyze Project');
 	});
