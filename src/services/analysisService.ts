40062aab3f9a9740a82571e294d1589b69e7c182
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-15 11:05:16 +0500

    refactor: remove AnalysisResult interface definition from analysisService

--- src/services/analysisService.ts
+++ src/services/analysisService.ts
@@ -1,7 +1,4 @@
-export interface AnalysisResult {
-    success: boolean;
-    message: string;
-}
+
 
 export class AnalysisService {
     async analyze(projectPath: string): Promise<AnalysisResult> {
