c6de702f0398af5d12bb1250b446795daa644fd2
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-16 10:30:06 +0500

    feat: implement resolveWebviewView method in AnalysisViewProvider

--- src/views/analysisViewProvider.ts
+++ src/views/analysisViewProvider.ts
@@ -2,5 +2,7 @@ import * as vscode from 'vscode';
 
 
 export class AnalysisViewProvider implements vscode.WebviewViewProvider {
-    
+    resolveWebviewView(webviewView: vscode.webviewView): void {
+        
+    }
 }
