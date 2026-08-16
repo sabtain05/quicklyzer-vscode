dd6d45f54dd1b2bad07f7e45786a89182a910e05
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-16 10:22:44 +0500

    fix: import AnalysisViewProvider in extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { ProjectService } from './services/projectService';
 import { AnalysisService } from './services/analysisService';
+import { AnalysisViewProvider } from './views/analysisViewProvider';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "quicklyzer" is now active!');
