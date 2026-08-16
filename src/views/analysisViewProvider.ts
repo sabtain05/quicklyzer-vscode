b537d02c17fd02c8c50a2881b21f2aa052537aab
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-16 10:31:36 +0500

    fix: correct type for webviewView parameter in resolveWebviewView method

--- src/views/analysisViewProvider.ts
+++ src/views/analysisViewProvider.ts
@@ -2,9 +2,9 @@ import * as vscode from 'vscode';
 
 
 export class AnalysisViewProvider implements vscode.WebviewViewProvider {
-    resolveWebviewView(webviewView: vscode.webviewView): void {
+    resolveWebviewView(webviewView: vscode.WebviewView): void {
         webviewView.webview.options = {
-            
-        }
+            enableScripts: false
+        };
     }
 }
