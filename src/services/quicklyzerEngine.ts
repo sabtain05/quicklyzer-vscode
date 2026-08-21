b98d115b4d6ce443da17c6c31ae810d301486526
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-21 10:03:29 +0500

    feat: add analyzeWorkspace function to utilize analyzeProject

--- src/services/quicklyzerEngine.ts
+++ src/services/quicklyzerEngine.ts
@@ -1,1 +1,5 @@
 import { analyzeProject } from "../../../quicklyzer/dist/services/project.js";
+
+export function analyzeWorkspace(workspacePath: string) {
+    return analyzeProject(workspacePath);
+}
