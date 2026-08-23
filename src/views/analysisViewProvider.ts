5a76cf81835f78f2b543cc2b8a3a880547f6b1df
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-23 20:19:46 +0500

    feat: improve escapeHtml method for better HTML escaping

--- src/views/analysisViewProvider.ts
+++ src/views/analysisViewProvider.ts
@@ -932,6 +932,6 @@ export class AnalysisViewProvider implements vscode.WebviewViewProvider {
     }
 
     private escapeHtml(value: any): string {
-        return value
+        return value.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;")
     }
 }
