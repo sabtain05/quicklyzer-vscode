50cfd4a10abcbf113107894f401d7219780e0f00
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-23 07:34:15 +0500

    feat: implement AnalysisViewProvider with initial view handling

--- src/views/analysisViewProvider.ts
+++ src/views/analysisViewProvider.ts
@@ -2,5 +2,9 @@ import * as vscode from "vscode";
 
 
 export class AnalysisViewProvider implements vscode.WebviewViewProvider {
-    
+    private view?: vscode.WebviewView;
+    resolveWebviewView(webviewView: vscode.WebviewView): void{
+        this.view = webviewView;
+    }
+        
 }
