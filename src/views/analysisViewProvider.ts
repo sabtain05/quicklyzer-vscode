769e964110eb0786ae9770f8c33dc9a35d21f8a3
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-23 18:43:11 +0500

    feat: enhance formatBytes method to handle values greater than 1 GB

--- src/views/analysisViewProvider.ts
+++ src/views/analysisViewProvider.ts
@@ -927,5 +927,7 @@ export class AnalysisViewProvider implements vscode.WebviewViewProvider {
         if(value < 1024 * 1024 * 1024) {
             return `${(value / (1024*1024)).toFixed(1)} MB`;
         }
+        
+        return `${(value / (1024*1024*1024)).toFixed(1)} GB`;
     }
 }
