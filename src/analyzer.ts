5298c5a467785f47ffe561a49e759ca14c3232b6
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-24 10:32:19 +0500

    feat: return project analysis result in analyzeWorkspace function

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -4,4 +4,6 @@ import * as path from "node:path";
 export async function analyzeWorkspace(workspacePath: string) {
     const quicklyzerPath = path.resolve("..", "quicklyzer");
     const projectModule = await import (path.join(quicklyzerPath,"dist","services","project.js"));
+    
+    return projectModule.analyzeProject(workspacePath);
 }
