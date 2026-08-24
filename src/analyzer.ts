5ec75f695e5efa6c1453b9d546b467c346edcb0b
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-24 10:31:27 +0500

    feat: import project module in analyzeWorkspace function for dynamic loading

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -3,5 +3,5 @@ import * as path from "node:path";
 
 export async function analyzeWorkspace(workspacePath: string) {
     const quicklyzerPath = path.resolve("..", "quicklyzer");
-    
+    const projectModule = await import (path.join(quicklyzerPath,"dist","services","project.js"));
 }
