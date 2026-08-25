009ef6950e359bb7f0cdbc3a2fb2608e5c538d5f
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-25 09:37:43 +0500

    refactor: simplify constructor and remove unused view property in QuicklyzerSidebarProvider

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -3,13 +3,11 @@ import * as vscode from "vscode";
 export class QuicklyzerSidebarProvider implements vscode.WebviewViewProvider {
   public static readonly viewType = "quicklyzer.dashboard";
 
-  private view?: vscode.WebviewView;
+ 
 
-  constructor(private readonly extensionUri: vscode.Uri) {}
+  constructor() {}
 
   resolveWebviewView(webviewView: vscode.WebviewView): void {
-    this.view = webviewView;
-
     webviewView.webview.options = {
       enableScripts: true,
     };
