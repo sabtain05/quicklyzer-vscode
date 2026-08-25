036815989e3585f8b7e98bc4466e9ea704cf6462
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-25 09:11:19 +0500

    feat: define viewType for QuicklyzerSidebarProvider class

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -2,5 +2,6 @@ import * as vscode from "vscode";
 
 
 export class QuicklyzerSidebarProvider implements vscode.WebviewViewProvider {
+    public static readonly viewType = "quicklyzer.dashboard";
     
 }
