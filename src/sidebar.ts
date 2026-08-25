bc02c14a2bae16e827aab66f3b2c55026f574f74
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-25 09:13:44 +0500

    feat: add constructor to QuicklyzerSidebarProvider class

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -4,4 +4,7 @@ import * as vscode from "vscode";
 export class QuicklyzerSidebarProvider implements vscode.WebviewViewProvider {
     public static readonly viewType = "quicklyzer.dashboard";
     private view?: vscode.WebviewView;
+
+    constructor(private readonly extensionUri: vscode.Uri) {}
+    
 }
