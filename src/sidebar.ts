8c797d0f4f661d96251d704b13f5026e6a010d81
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-25 09:12:00 +0500

    feat: add private view property to QuicklyzerSidebarProvider class

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -3,5 +3,5 @@ import * as vscode from "vscode";
 
 export class QuicklyzerSidebarProvider implements vscode.WebviewViewProvider {
     public static readonly viewType = "quicklyzer.dashboard";
-    
+    private view?: vscode.WebviewView;
 }
